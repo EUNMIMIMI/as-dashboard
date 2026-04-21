011ea9bb6543e94139ee92b34e7f180e20b83acd
eunmi <roqlrotns97@gmail.com>
2026-04-21 10:03:44 +0900

    @

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ const firebaseConfig = isCanvasEnv
       messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
       appId: import.meta.env.VITE_FIREBASE_APP_ID
     };
-
+    
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
